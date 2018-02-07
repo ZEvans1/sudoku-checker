161e6d2337a8c662f62629ce08cf6126ad8e1bf8
Guest User <Guest@Epicodus-4D.local>
2018-02-06 16:11:00 -0800

    Add code for jasmine / karma.

---
+++ spec/checker-spec.js
@@ -0,0 +1,10 @@
+var Checker = require('./../js/checker.js').checkerModule;
+
+describe('Checker', function() {
+
+  it('should test if array is full', function() {
+
+  });
+});
+
+exports.checkerModule = Checker;
